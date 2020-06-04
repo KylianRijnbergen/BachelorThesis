b874fb16346793319b8dfcbdc17caa775882062a
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-06-04 15:22:24 +0200

    Added comments

--- Email_Extraction/Script_Email_Processing.py
+++ Email_Extraction/Script_Email_Processing.py
@@ -5,10 +5,9 @@ import base64 #Used for encoding/decoding of base64 strings
 def decodeBase64(encodedStr):
 	if(encodedStr[:10] == '=?UTF-8?B?' and encodedStr[-2:] == '?='): #only base64 encoded strings
 		encodedStr	= encodedStr[10:-2] #Removes first 10 and last 2 characters
-		decodedBytes = base64.b64decode(encodedStr)
-		decodedStr = str(decodedBytes, "utf-8")
-		return decodedStr
+		decodedBytes = base64.b64decode(encodedStr) #Decoding to base 64
+		decodedStr = str(decodedBytes, "utf-8") #UTF-8 string
+		return decodedStr #returns decoded string
 	else:
-		return encodedStr
-
+		return encodedStr #returns encoded string
 
