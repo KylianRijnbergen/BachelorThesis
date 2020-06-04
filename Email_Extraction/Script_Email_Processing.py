93ca15cdd275e64524ba1eb47fa7c2dbdd9a541f
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-06-04 15:19:47 +0200

    added base 64 decoding

--- Email_Extraction/Script_Email_Processing.py
+++ Email_Extraction/Script_Email_Processing.py
@@ -1,5 +1,14 @@
-# Import libraries
-import numpy as np
+# Import 
+import base64 #Used for encoding/decoding of base64 strings
+
 
 def decodeBase64(encodedStr):
 	if(encodedStr[:10] == '=?UTF-8?B?' and encodedStr[-2:] == '?='): #only base64 encoded strings
+		encodedStr	= encodedStr[10:-2] #Removes first 10 and last 2 characters
+		decodedBytes = base64.b64decode(encodedStr)
+		decodedStr = str(decodedBytes, "utf-8")
+		return decodedStr
+	else:
+		return encodedStr
+
+
