e038682b279ab3999b133d1b6ea95d8d735392d7
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-06-05 15:00:01 +0200

    Added a function that converts HTML to plain text.

--- Email_Extraction/Script_Email_Processing.py
+++ Email_Extraction/Script_Email_Processing.py
@@ -2,6 +2,7 @@
 import base64 #Used for encoding/decoding of base64 strings
 import numpy as np #Numpy library for arrays, tables and datastructures.
 from nltk.parse import CoreNLPParser
+import html2text as h2t
 
 def decodeBase64(encodedStr):
 	if(encodedStr[:10] == '=?UTF-8?B?' and encodedStr[-2:] == '?='): #only base64 encoded strings
@@ -24,3 +25,7 @@ def NER(word):
 	ner_tagger = CoreNLPParser(url='http://localhost:9000', tagtype='ner')
 	type = ner_tagger.tag(word.split())
 	return type
+
+def HTML_To_Text(HTML_String):
+    Plain_Text = h2t.html2text(HTML_String)
+    return Plain_Text
