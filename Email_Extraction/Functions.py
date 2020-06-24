9ebaabf2b3de9fff35fb5aebb2c175d9dc2c82df
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-06-24 16:49:37 +0200

    Added functions for text preprocessing

--- Email_Extraction/Functions.py
+++ Email_Extraction/Functions.py
@@ -3,10 +3,25 @@ import base64 #Used for encoding/decoding of base64 strings
 import numpy as np #Numpy library for arrays, tables and datastructures.
 from nltk.parse import CoreNLPParser
 import html2text as h2t
-import datetime
 import re as regex
+from collections import Counter
+import pandas as pd
 
 
+def ListToString(List, Delimiter = " "):
+	if isinstance(List, list):
+		return Delimiter.join(List)
+	else:
+		return List
+    
+def StringToList(List, Delimiter = " "):
+	if isinstance(List, str):
+		return List.split(Delimiter)
+	else: 
+		return List
+    
+# Decoding functions
+#Base 64 ("=?utf-8?b?" && "?=")
 def decodeBase64(encodedStr):
 	if(encodedStr[:10].lower() == '=?utf-8?b?' and encodedStr[-2:] == '?='): #only base64 encoded strings
 		encodedStr	= encodedStr[10:-2] #Removes first 10 and last 2 characters
@@ -16,24 +31,35 @@ def decodeBase64(encodedStr):
 	else:
 		return encodedStr #returns encoded string
 
-def split(string_to_split_array):
-	splittedArray = np.empty(0, dtype=str)
-	splits = string_to_split_array.split()
-	for split in splits:
-		splittedArray = np.append(splittedArray, split)
-	return splittedArray
-
 #The NER Tagger (part of CoreNLP) can be started by running Start_Server.bat in 'D:\Bachelor_Thesis\Email_Extraction\CoreNLP\stanford-corenlp-4.0.0'
 def NER(word):
 	ner_tagger = CoreNLPParser(url='http://localhost:9000', tagtype='ner')
-	type = ner_tagger.tag(word.split())
-	return type
+	Linguistic_Element_Type = ner_tagger.tag(word.split())
+	return Linguistic_Element_Type
 
 def HTML_To_Text(HTML_String):
     Plain_Text = h2t.html2text(HTML_String)
     return Plain_Text
 
+#Fetch Email Address
 def Retreive_Email_Addresses(Email_String):
-        regex_Email_Address = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' #regex to recognise email address
-        Email = regex.findall(regex_Email_Address, str(Email_String))
-        return Email
+    regex_Email_Address = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' #regex to recognise email address
+    Email = regex.findall(regex_Email_Address, str(Email_String))
+    Email_No_Dupes = RemoveDuplicates(Email)
+    return Email_No_Dupes
+
+#Remove Email Address, return username (everything that is left)
+def Get_Email_Username(Email_String, Email_Address):
+	List_Sender_Email = StringToList(Email_String)
+	#Remove duplicates using function below
+	if List_Sender_Email[-1] == Email_Address: #check if last element is email address
+		del List_Sender_Email[-1]
+	return List_Sender_Email
+
+def RemoveDuplicates(List_Unfiltered):
+    DuplicatesRemoved = list(Counter(List_Unfiltered))
+    return DuplicatesRemoved
+    
+def CutDataFrame(DataFrame_Name, Startrow, Endrow):
+	Sliced_DataFrame = DataFrame_Name[Startrow: Endrow]
+	return Sliced_DataFrame
--- Email_Extraction/Load_Data_To_DataFrame.py
+++ Email_Extraction/Load_Data_To_DataFrame.py
@@ -6,67 +6,79 @@ Created on Tue Jun  9 09:27:28 2020
 """
 
 import pandas as pd #Pandas library for DataFrames
-import re as regex #Import Regular Expressions
 import Functions as Fn #Import Functions from Functions.py 
+import random as rand #Importing random library
 
-
-
-Directory_Phishing_Mails = "D:/Bachelor_Thesis/APWG Phishing Emails/APWG Phishing Emails/" #Directory where JSON files are stored.
-
-#Mail files contained in Directory_Phishing_Mails. If Load_All is set to True, all files are loaded. If False, only "phish_month_8_2018" is loaded. This is the smallest file.
+#Declaring debugging / setting variables
+#Directory_Phishing_Mails = "D:/Bachelor_Thesis/APWG Phishing Emails/APWG Phishing Emails/" #Directory where JSON files are stored.
+Directory_Phishing_Mails = "D:/Bachelor_Thesis/Email_Extraction/"
+Reload = True
 Load_All = False
-if Load_All:
-	List_Mail_Files = ["phish_month_8_2018", "phish_month_9_2018", "phish_month_10_2018", "phish_month_11_2018", "phish_month_12_2018", "phish_month_1_2019", "phish_month_2_2019", "phish_month_3_2019"]
+Headers_All = True
+To_Csv = True
+
+
+#Reloading of dataframe.
+if Reload: 
+    Df_Raw_Data = pd.DataFrame() #Empty dataframe where our data will be stored.
+    #Mail files contained in Directory_Phishing_Mails. If Load_All is set to True, all files are loaded. If False, only "phish_month_8_2018" is loaded. This is the smallest file.
+    if Load_All:
+        List_Mail_Files = ["phish_month_8_2018", "phish_month_9_2018", "phish_month_10_2018", "phish_month_11_2018", "phish_month_12_2018", "phish_month_1_2019", "phish_month_2_2019", "phish_month_3_2019"]
+    else:
+        List_Mail_Files = ["Debugging_File_100_Rows_Only"]	
+    #Load files and add contents to Pandas Dataframe "Df_Raw_Data".
+    for Filename in List_Mail_Files:
+        with open(Directory_Phishing_Mails + Filename + ".json") as File_To_Load:
+            Df_Raw_File_Data = pd.read_json(File_To_Load)
+            Df_Raw_Data = Df_Raw_Data.append(Df_Raw_File_Data)
+        del Df_Raw_File_Data
+
+print("Start!")
+#Lines below create a debugging file.
+#Debugging_File = Fn.CutDataFrame(Df_Raw_Data, 0, 100)
+#Debugging_File.to_json("D:/Bachelor_Thesis/Email_Extraction/Debugging_File_100_Rows_Only.json")
+
+#If Headers_All is set to true, all headers are loaded. If False, only alternative is loaded.
+if Headers_All:
+    List_Data_Headers = ["id", "date_sent", "date_received", "date_reported", "links", "sender_email", "recipient_email", "email_subject", "email_raw_headers", "email_raw_body", "email_has_attachments", "modified"] #List of all headers
 else:
-	List_Mail_Files = ["phish_month_12_2018"]
+    List_Data_Headers = ["sender_email"]
+List_Data_Dates = ["date_sent", "date_received", "date_reported"] #List for all headers where the data has the format "timestamp". Datatype is np.int64
 
 
-Df_Raw_Data = pd.DataFrame() #Empty dataframe where our data will be stored.
+for Header in List_Data_Headers: #Loop over all Columns
+    for Index in range(0, len(Df_Raw_Data)): #Loop over all Rows.
+        Data = Df_Raw_Data.loc[Index,Header] #Retreive value and assign it to "Data".
+        
+        #Assign random values for IsPhishing. Debugging purposes only.
+        if rand.random() > 0.1:
+            Df_Raw_Data.loc[Index, "IsPhishing"] = 0 
+       	else:
+       		Df_Raw_Data.loc[Index, "IsPhishing"] = 1
+        	#print(str(Index) + " " + Header) #Prints current Column and Row for Debugging purposes.
 
-#Load files and add contents to Pandas Dataframe "Df_Raw_Data".
-for Filename in List_Mail_Files:
-	with open(Directory_Phishing_Mails + Filename + ".json") as File_To_Load:
-		Df_Raw_File_Data = pd.read_json(File_To_Load)
-		Df_Raw_Data = Df_Raw_Data.append(Df_Raw_File_Data)
+        if Header in List_Data_Dates: #Checks if Data is in format Timestamp by comparing Headers.
+            if Data > 0: #Timestamp has to be positive. This is not the case in the raw data, this needs to be addressed.
+                Data = pd.datetime.fromtimestamp(Data) #Convert Timestamp to DateTime format using Pandas.
+                Df_Raw_Data.loc[Index, Header] = Data #Assigns DateTime format to Cell in Df_Raw_Data.
+        
+        elif Header == "sender_email": #Checks if entry may contain email address.
+            Email_Address = Fn.Retreive_Email_Addresses(Data)
+            if len(Email_Address) == 0:
+                Email_Address = [""]
+                
+            Df_Raw_Data.loc[Index, "Sender_Email_Filtered"] = Email_Address[-1]
+            Email_Username = Fn.Get_Email_Username(Data, Fn.ListToString(Email_Address[-1]))
+            
+               
+            
+            if len(Email_Username) != 0:
+                Df_Raw_Data.loc[Index, "Sender_Email_Username"] = Fn.ListToString(Email_Username, " ")
+            else:
+                Df_Raw_Data.loc[Index, "Sender_Email_Username"] = ""
 
-	del Df_Raw_File_Data
-
-	Df_Raw_Data["Email_Filtered"] = ""
-	Mistakes = 0
-	print("Start!")
-	#If Headers_All is set to true, all headers are loaded. If False, only alternative is loaded.
-	Headers_All = True
-	if Headers_All:
-	    List_Data_Headers = ["id", "date_sent", "date_received", "date_reported", "links", "sender_email", "recipient_email", "email_subject", "email_raw_headers", "email_raw_body", "email_has_attachments", "modified"] #List of all headers
-	else:
-	    List_Data_Headers = ["sender_email"]
-
-	List_Data_Dates = ["date_sent", "date_received", "date_reported"] #List for all headers where the data has the format "timestamp". Datatype is np.int64
-
-	for Header in List_Data_Headers: #Loop over all Columns
-	    for Index in range(0, len(Df_Raw_Data)): #Loop over all Rows.
-	        Data = Df_Raw_Data.loc[Index,Header] #Retreive value and assign it to "Data".
-	        #print(str(Index) + " " + Header) #Prints current Column and Row for Debugging purposes.
-	        
-	        if Header in List_Data_Dates: #Checks if Data is in format Timestamp by comparing Headers.
-	            if Data > 0: #Timestamp has to be positive. This is not the case in the raw data, this needs to be addressed.
-	                Data = pd.datetime.fromtimestamp(Data) #Convert Timestamp to DateTime format using Pandas.
-	                Df_Raw_Data.loc[Index, Header] = Data #Assigns DateTime format to Cell in Df_Raw_Data.
-	        elif Header == "sender_email": #Checks if entry may contain email address.
-	            Mail_Address = Fn.Retreive_Email_Addresses(Data)
-	            if len(Mail_Address) == 1:
-	                Df_Raw_Data.loc[Index, "Email_Filtered"] = Mail_Address[0]
-	            elif len(Mail_Address) == 0:
-	                Df_Raw_Data.loc[Index, "Email_Filtered"] = "Something went wrong. No Email Address was found."
-	            elif Mail_Address[0] == Mail_Address[len(Mail_Address) - 1]:
-	                Df_Raw_Data.loc[Index, "Email_Filtered"] = Mail_Address[0]
-	                
-	            
-	            
-	    
-	        
-
-
-	#Write to CSV
-	if True:
-	    Df_Raw_Data.to_csv("D:/Bachelor_Thesis/Email_Extraction/Raw_Data_Csv_File_" + Filename + ".csv")
+          
+Df_Raw_Data = Df_Raw_Data.drop(columns = ["links"])
+#Write to CSV 
+if To_Csv:
+    Df_Raw_Data.to_csv("D:/Bachelor_Thesis/Email_Extraction/Raw_Data_Csv_File_" + Filename + ".csv")
