052a91ad7959ebf4621f5ed7b9b80c243361b0a6
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-07-20 12:36:45 +0200

    Changed self-made function to default .split()

--- Word_Embeddings/Bag_of_Words.py
+++ Word_Embeddings/Bag_of_Words.py
@@ -6,12 +6,15 @@ Created on Mon Jul 13 10:26:33 2020
 """
 
 from sklearn.feature_extraction.text import CountVectorizer
+    
 
 class TextToBow:
+    
+    """ converts string to bag of words """
     
     def __init__(self, mail_string):
         
         self.string = mail_string
         vectorizer = CountVectorizer()
-        self.matrix = vectorizer.fit_transform(StringToList(mail_string))
+        self.matrix = vectorizer.fit_transform(mail_string.split(" "))
         self.tokens = vectorizer.get_feature_names()
