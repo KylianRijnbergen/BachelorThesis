4474db0986a4174dfe1a7309e475b89d3c2b666a
KylianRijnbergen <66416875+KylianRijnbergen@users.noreply.github.com>
2020-07-25 11:23:43 +0200

    Updated file references.

--- Email_Extraction/Machine_Learning_All_Algorithms.py
+++ Email_Extraction/Machine_Learning_All_Algorithms.py
@@ -21,51 +21,54 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from class_performance_metrics import PerformanceMetrics
+import pickle
 
-
+ 
 #Selecting which files to load.
 Directory_Labelled_Emails = "D:/Bachelor_Thesis/Labelled_Emails_Features_Only/Word_Vectors/"
-List_Mail_Files =  ["300_Dimensions_Weighted"]	
+List_Mail_Files =  ["nan_removed"]	
 
 Df_Data = pd.DataFrame()
-#Load files and add contents to Pandas Dataframe "Df_Raw_Data".
-for Filename in List_Mail_Files:
-    with open(Directory_Labelled_Emails + Filename + ".csv") as File_To_Load:
-        Df_Raw_File_Data = pd.read_csv(File_To_Load)
-        Df_Data = Df_Data.append(Df_Raw_File_Data)
-    del Df_Raw_File_Data
+load_Augmented = True #True loads augmented, false loads normal.
 
-
-  
-del Directory_Labelled_Emails
-del Filename
-del List_Mail_Files
-
+if load_Augmented == True:
+    #Load files and add contents to Pandas Dataframe "Df_Raw_Data".
+    for Filename in List_Mail_Files:
+        with open(Directory_Labelled_Emails + Filename + ".csv") as File_To_Load:
+            Df_Raw_File_Data = pd.read_csv(File_To_Load)
+            Df_Data = Df_Data.append(Df_Raw_File_Data)
+        del Df_Raw_File_Data
+    
+    
+      
+    del Directory_Labelled_Emails
+    del Filename
+    del List_Mail_Files
+else:
+    Df_Data = pd.read_excel("D:/Bachelor_Thesis/Labelled_Emails_Features_Only/Word_Vectors/300_Dimensions.xlsx", index_col = None)
 
 Np_Data = Df_Data.to_numpy()
 
-
-Np_Data = np.delete(Np_Data, 99, axis = 0) # This corrects a bug where a certain dataframe entry contains "NaN" values.
 
 Np_Feature_Vectors = np.delete(Np_Data, [0, 1, 2], axis = 1)
 
 Labels = DfFun.GetColumnFromDataFrame(Df_Data, "Label")
 Np_Labels = Labels.to_numpy()
 
-Np_Labels = np.delete(Np_Labels, 99, axis = 0)
 
-Accuracy = 0
+
+##### TRAINING PART
 
 Start = 0
-End = 250
+End = 5
 
 
 classifiers = [
-        svm.SVC(kernel = "rbf", gamma = "scale"),
-        RandomForestClassifier(n_estimators = 1000, max_depth = 2),
-        MLPClassifier(solver = "lbfgs", alpha = 0.3, hidden_layer_sizes = (500,20)),
-        LogisticRegression(solver = "lbfgs"),
-        AdaBoostClassifier(n_estimators=5, learning_rate=1)
+        #svm.SVC(kernel = "rbf", gamma = "scale"),
+        #RandomForestClassifier(n_estimators = 1000, max_depth = 2),
+        MLPClassifier(solver = "lbfgs", alpha = 0.0005, hidden_layer_sizes = (5000,200)),
+        #LogisticRegression(solver = "lbfgs"),
+        #AdaBoostClassifier(n_estimators=5, learning_rate=1)
         ]
 
 TEST_SIZE = 0.2
@@ -76,7 +79,8 @@ SCORE_ATTRIBUTES = [
         "accuracy",
         "precision",
         "recall",
-        "f1_score"]
+        "f1_score"
+        ]
 df_scores = pd.DataFrame(columns = SCORE_ATTRIBUTES)
 for index in range(Start, End):
     print(index)
@@ -85,10 +89,13 @@ for index in range(Start, End):
         clf_position = classifiers.index(clf)
         clf.fit(X_train, y_train.ravel())
         y_pred = clf.predict(X_test)
-        scores_for_run = PerformanceMetrics(clf, index, TEST_SIZE, y_test, y_pred)
+        scores_for_run = PerformanceMetrics(clf, index, y_test, y_pred, TEST_SIZE)
         for attribute, value in scores_for_run.__dict__.items():
             df_scores.loc[index + (End-Start) * clf_position, attribute] = value
             
 df_scores = df_scores.sort_index()
         
 best_score = df_scores["accuracy"].max()    
+
+print(best_score)
+
